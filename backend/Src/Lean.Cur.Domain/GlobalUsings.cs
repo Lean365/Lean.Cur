3753ae5591767223e0d17fd27099401de78d161a
Lean365 <leansoft365@outlook.com>
2025-01-11 18:03:40 +0800

    feat: 添加Domain层全局using，更新项目依赖和配置

---
+++ backend/Src/Lean.Cur.Domain/GlobalUsings.cs
@@ -0,0 +1,21 @@
+// 系统命名空间
+global using System;
+global using System.Collections.Generic;
+global using System.ComponentModel;
+global using System.ComponentModel.DataAnnotations;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+
+// Microsoft AspNetCore
+global using Microsoft.AspNetCore.Mvc;
+
+// 项目命名空间
+global using Lean.Cur.Common.Attributes;
+global using Lean.Cur.Common.Configs;
+global using Lean.Cur.Common.Constants;
+global using Lean.Cur.Common.Enums;
+global using Lean.Cur.Common.Extensions;
+global using Lean.Cur.Common.Helpers;
+global using Lean.Cur.Common.Models;
+global using Lean.Cur.Common.Utils;
